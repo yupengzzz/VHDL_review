b538a15e11a1f6bd7307ffe045159b2cdf2ee266
Yupeng Zhao <39612917+yupengzzz@users.noreply.github.com>
2018-10-13 22:05:59 -0700

    Create registerSequencer.vhd

---
+++ registerSequencer.vhd
@@ -0,0 +1,12 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+--	the following register will be loaded with new inputs
+--when enable is 1, or rotate as a sequencer when enable is 0
+entity Register1 is
+	port(enable : in std_logic;
+		D : in std_logic_vector(3 downto 0);
+		Q : out std_logic_vector(3 downto 0));
+end Register1;
+
+architecture
