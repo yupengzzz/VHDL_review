98ab65f25328c6e167b6056b8451c08bc48c0ee7
Yupeng Zhao <39612917+yupengzzz@users.noreply.github.com>
2018-10-13 22:13:26 -0700

    Update registerSequencer.vhd

--- registerSequencer.vhd
+++ registerSequencer.vhd
@@ -4,11 +4,15 @@ use ieee.std_logic_1164.all;
 --	the following register will be loaded with new inputs
 --when enable is 1, or rotate as a sequencer when enable is 0
 entity Register1 is
-	port(enable : in std_logic;
+	port(enable, Clock : in std_logic;
 		D : in std_logic_vector(3 downto 0);
 		Q : out std_logic_vector(3 downto 0));
 end Register1;
 
 architecture behavior of Register is
-	
+	signal MuxOut : std_logic_vector(3 downto 0);
+begin
+	Q <= MuxOut when Rising_Edge(Clock);
+	MuxOut <= D when enable = '1' else
+		Q(0) & Q(3 downto 1);
 end behavior;
